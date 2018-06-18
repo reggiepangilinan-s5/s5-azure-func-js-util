84b0914ee0d1a93a7d435418ddf41df0d1b2be94
reggiepangilinan-s5 <reggie.pangilinan@stationfive.com>
2018-06-18 12:07:22 +1000

    Add dist/index.js as main entry point

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var constants = require('./constants');
+var handlers = require('./handlers');
+var messages = require('./messages');
+var validations = require('./validations');
+
+module.exports = {
+  constants: constants,
+  handlers: handlers,
+  messages: messages,
+  validations: validations
+};
+//# sourceMappingURL=index.js.map
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const constants = require('./constants');
+const handlers = require('./handlers');
+const messages = require('./messages');
+const validations = require('./validations');
+
+module.exports = {
+  constants,
+  handlers,
+  messages,
+  validations,
+};
