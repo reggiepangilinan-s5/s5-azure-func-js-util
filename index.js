504ef03967e47f59fd48787f0f62169ec325f55b
reggiepangilinan-s5 <reggie.pangilinan@stationfive.com>
2018-07-05 14:05:47 +1000

    Export module servicebus

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ var constants = require('./constants');
 var handlers = require('./handlers');
 var messages = require('./messages');
 var validations = require('./validations');
+var servicebus = require('./servicebus');
 
 module.exports = {
   constants: constants,
   handlers: handlers,
   messages: messages,
-  validations: validations
+  validations: validations,
+  servicebus: servicebus
 };
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const constants = require('./constants');
 const handlers = require('./handlers');
 const messages = require('./messages');
 const validations = require('./validations');
+const servicebus = require('./servicebus');
 
 module.exports = {
   constants,
   handlers,
   messages,
   validations,
+  servicebus,
 };
